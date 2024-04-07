9dc52d38093e67e953bcf6188533feae8cada14b
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-04-07 14:04:44 -0700

    Translate - aws sdk test

--- localizer/src/App.js
+++ localizer/src/App.js
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
 import TranslationComponent from './components/TranslationComponent';
+
 import './App.css'
+import {
+  TranslateClient,
+  TranslateTextCommand,
+} from "@aws-sdk/client-translate";
+
 
 const App = ()=>{
   const [language, setLanguage] = useState('en');
@@ -10,6 +16,29 @@ const App = ()=>{
   }
 
 
+
+ const handleTranslation = async (textAndSourceLanguage) => {
+    const translateClient = new TranslateClient({ 
+  region: "ap-southeast-1",
+  credentials: {
+    accessKeyId: '[access-key-id]', 
+    secretAccessKey: '[secret-access-key]',
+  }
+});
+  
+    const translateCommand = new TranslateTextCommand({
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "fr",
+      Text: "This is a sample text",
+    });
+  
+    const { TranslatedText } = await translateClient.send(translateCommand);
+    console.log(TranslatedText)
+    return { translated_text: TranslatedText };
+  };
+
+handleTranslation('en')
+// console.log(fetchTranslations('fr'))
 const translations = require(`../public/translations/${language}.json`)
 
 return (
