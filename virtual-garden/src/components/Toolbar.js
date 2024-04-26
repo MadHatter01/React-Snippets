afd49b2377bbdb5a2fb20c7ae9d9a1aa414c7dc2
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-04-25 18:35:50 -0700

    styling button

--- virtual-garden/src/components/Toolbar.js
+++ virtual-garden/src/components/Toolbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Toolbar = ({ onAddElement }) => {
   return (
     <div className="toolbar">
-      <button onClick={() => onAddElement('flower')}>Add Flower</button>
-      <button onClick={() => onAddElement('tree')}>Add Tree</button>
-      <button onClick={() => onAddElement('decoration')}>Add Decoration</button>
+      <button className="primary-btn" onClick={() => onAddElement('flower')} >Add Flower</button>
+      <button className="primary-btn" onClick={() => onAddElement('tree')}>Add Tree</button>
+      <button className="primary-btn" onClick={() => onAddElement('decoration')}>Add Decoration</button>
     </div>
   );
 };
